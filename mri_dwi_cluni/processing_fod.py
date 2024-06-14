ca8629abcda3115b6f2a2f5878a22d11fe7f7e9f
manuegrx <47973414+manuegrx@users.noreply.github.com>
2024-06-14 17:33:56 +0200

    add response and fod for single shell

--- mri_dwi_cluni/processing_fod.py
+++ mri_dwi_cluni/processing_fod.py
@@ -19,31 +19,31 @@ def run_processing_fod(in_dwi, brain_mask):
     mylog = logging.getLogger("custom_logger")
     mylog.info("Launch processing FOD")
 
-    # DWI response
-    wm = os.path.join(dir_name, "response_wm.txt")
-    gm = os.path.join(dir_name, "response_gm.txt")
-    csf = os.path.join(dir_name, "response_csf.txt")
-    voxels = os.path.join(dir_name, "response_voxels.mif")
-    cmd = [
-        "dwi2response",
-        "dhollander",
-        in_dwi,
-        wm,
-        gm,
-        csf,
-        "-voxels",
-        voxels,
-    ]
-    result, stderrl, sdtoutl = execute_command(cmd)
-    if result != 0:
-        msg = f"Can not lunch dwi2response (exit code {result})"
-        return 0, msg, info
-
-    # FOD
+    # Different processing if multi-shell or single shell
     result, msg, shell = get_shell(in_dwi)
     shell = [bval for bval in shell if bval != "0" and bval != ""]
-    wm_fod = os.path.join(dir_name, "wmfod.mif")
     if len(shell) > 1:
+        # DWI response
+        wm = os.path.join(dir_name, "response_wm.txt")
+        gm = os.path.join(dir_name, "response_gm.txt")
+        csf = os.path.join(dir_name, "response_csf.txt")
+        voxels = os.path.join(dir_name, "response_voxels.mif")
+        cmd = [
+            "dwi2response",
+            "dhollander",
+            in_dwi,
+            wm,
+            gm,
+            csf,
+            "-voxels",
+            voxels,
+        ]
+        result, stderrl, sdtoutl = execute_command(cmd)
+        if result != 0:
+            msg = f"Can not lunch dwi2response (exit code {result})"
+            return 0, msg, info
+        # FOD
+        wm_fod = os.path.join(dir_name, "wmfod.mif")
         gm_fod = os.path.join(dir_name, "gmfod.mif")
         csf_fod = os.path.join(dir_name, "csffod.mif")
         cmd = [
@@ -85,6 +85,15 @@ def run_processing_fod(in_dwi, brain_mask):
             return 0, msg, info
         wm_fod = wm_fod_norm
     else:
+        # DWI response
+        wm = os.path.join(dir_name, "response_wm.txt")
+        cmd = ["dwi2response", "tournier", in_dwi, wm]
+        result, stderrl, sdtoutl = execute_command(cmd)
+        if result != 0:
+            msg = f"Can not lunch dwi2response (exit code {result})"
+            return 0, msg, info
+        # FOD
+        wm_fod = os.path.join(dir_name, "wmfod.mif")
         cmd = ["dwi2fod", "csd", in_dwi, "-mask", brain_mask, wm, wm_fod]
         result, stderrl, sdtoutl = execute_command(cmd)
         if result != 0:
