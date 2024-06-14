3b7e01cdf62d2ebb4d4f991e38d316e58abbc227
manuegrx <47973414+manuegrx@users.noreply.github.com>
2024-06-14 15:06:41 +0200

    add possibility to have not multishell data

--- mri_dwi_cluni/processing_fod.py
+++ mri_dwi_cluni/processing_fod.py
@@ -6,7 +6,7 @@ Functions to get FOD from DWI data:
 import logging
 import os
 
-from useful import execute_command
+from useful import execute_command, get_shell
 
 
 def run_processing_fod(in_dwi, brain_mask):
@@ -40,50 +40,60 @@ def run_processing_fod(in_dwi, brain_mask):
         return 0, msg, info
 
     # FOD
+    result, msg, shell = get_shell(in_dwi)
+    shell = [bval for bval in shell if bval != "0" and bval != ""]
     wm_fod = os.path.join(dir_name, "wmfod.mif")
-    gm_fod = os.path.join(dir_name, "gmfod.mif")
-    csf_fod = os.path.join(dir_name, "csffod.mif")
-    cmd = [
-        "dwi2fod",
-        "msmt_csd",
-        in_dwi,
-        "-mask",
-        brain_mask,
-        wm,
-        wm_fod,
-        gm,
-        gm_fod,
-        csf,
-        csf_fod,
-    ]
-    result, stderrl, sdtoutl = execute_command(cmd)
-    if result != 0:
-        msg = f"Can not lunch FOD (exit code {result})"
-        return 0, msg, info
+    if len(shell) > 1:
+        gm_fod = os.path.join(dir_name, "gmfod.mif")
+        csf_fod = os.path.join(dir_name, "csffod.mif")
+        cmd = [
+            "dwi2fod",
+            "msmt_csd",
+            in_dwi,
+            "-mask",
+            brain_mask,
+            wm,
+            wm_fod,
+            gm,
+            gm_fod,
+            csf,
+            csf_fod,
+        ]
+        result, stderrl, sdtoutl = execute_command(cmd)
+        if result != 0:
+            msg = f"Can not lunch FOD (exit code {result})"
+            return 0, msg, info
 
-    # Normalise
-    wm_fod_norm = os.path.join(dir_name, "wmfod_norm.mif")
-    gm_fod_norm = os.path.join(dir_name, "gmfod_norm.mif")
-    csf_fod_norm = os.path.join(dir_name, "csffod_norm.mif")
-    cmd = [
-        "mtnormalise",
-        wm_fod,
-        wm_fod_norm,
-        gm_fod,
-        gm_fod_norm,
-        csf_fod,
-        csf_fod_norm,
-        "-mask",
-        brain_mask,
-    ]
-    result, stderrl, sdtoutl = execute_command(cmd)
-    if result != 0:
-        msg = f"Can not lunch normalise (exit code {result})"
-        return 0, msg, info
+        # Normalise
+        wm_fod_norm = os.path.join(dir_name, "wmfod_norm.mif")
+        gm_fod_norm = os.path.join(dir_name, "gmfod_norm.mif")
+        csf_fod_norm = os.path.join(dir_name, "csffod_norm.mif")
+        cmd = [
+            "mtnormalise",
+            wm_fod,
+            wm_fod_norm,
+            gm_fod,
+            gm_fod_norm,
+            csf_fod,
+            csf_fod_norm,
+            "-mask",
+            brain_mask,
+        ]
+        result, stderrl, sdtoutl = execute_command(cmd)
+        if result != 0:
+            msg = f"Can not lunch normalise (exit code {result})"
+            return 0, msg, info
+        wm_fod = wm_fod_norm
+    else:
+        cmd = ["dwi2fod", "csd", in_dwi, "-mask", brain_mask, wm, wm_fod]
+        result, stderrl, sdtoutl = execute_command(cmd)
+        if result != 0:
+            msg = f"Can not lunch FOD (exit code {result})"
+            return 0, msg, info
 
     # Extract peaks
     peaks = os.path.join(dir_name, "peaks.mif")
-    cmd = ["sh2peaks", wm_fod_norm, peaks]
+    cmd = ["sh2peaks", wm_fod, peaks]
     result, stderrl, sdtoutl = execute_command(cmd)
     if result != 0:
         msg = f"Can not lunch sh2peaks (exit code {result})"
